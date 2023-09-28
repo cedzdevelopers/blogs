640e04502d56ed340836d7b33e051d4b54a247d5
yakkshit <saiyakkshit2001@gmail.com>
2023-09-28 13:14:27 +0200

    1st commit

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,28 @@
+export const navLinks = [
+  {
+    label: 'Blogs',
+    pathname: '/articles',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/yakkshit',
+    icon: 'twitter',
+  },
+  {
+    label: 'Figma',
+    url: 'https://www.linkedin.com/in/yakkshit',
+    icon: 'figma',
+  },
+  {
+    label: 'Github',
+    url: 'https://github.com/yakkshit',
+    icon: 'github',
+  },
+];
